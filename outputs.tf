6ee47f10e98cf3b64bb6f59509ef412aeb985eaa
Hazem mohamed <96701230+7azemmm@users.noreply.github.com>
2025-10-16 14:00:07 +0300

    Reformat outputs.tf for consistency

--- outputs.tf
+++ outputs.tf
@@ -21,7 +21,3 @@ output "wordpress_sg_id" {
   value = module.wordpress.wordpress_sg
 }
 
-# output "wordpress_public_ips" {
-#   description = "Public IPs of WordPress instances"
-#   value       = module.wordpress.wordpress_instance_public_ips
-# }
